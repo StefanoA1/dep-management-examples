8b3b113014a41a4165f5ff0b17ed5f7476ddcfc2
Stefano Acosta <33550261+StefanoA1@users.noreply.github.com>
2021-02-05 18:56:34 +0100

    Update workshop.ts

--- workshop.ts
+++ workshop.ts
@@ -228,10 +228,12 @@ Pros:
 - Initialement ça peut prendre plus de temps mais le code devient très flexible pour les modifications
 - Tout le code devient testable, le code d'infra/IO est injecté, donc pas compliqué à tester en TU
 - La partie I/O devient plus claire à comprendre
+- Il est facile de les coupler avec d’autres types d'implémentations
 
 Cons:
 - Prends plus de temps initialement pour mettre en place
-
+- Multiplie le nombre d'interfaces nécessaires
+- Les fonctions deviennent gonflées de paramètres
 */
 
 const pureUpdateCustomerProfileDP = (
@@ -341,24 +343,20 @@ const updateCustomerProfileDP2 = (services: IServices, dbConnection: DbConnectio
 // Reader monad
 // Delaying the injection of dependencies
 
-// Simple reader Monad
-// function reader(k: Function) {
-//   return {
-//     run: (e: Function) => {
-//       return k(e);
-//     },
-//     bind: (f: Function) => {
-//       return reader(function (e: Function) {
-//         return f(k(e)).run(e);
-//       });
-//     },
-//     map: (f: Function) => {
-//       return reader(function (e: Function) {
-//         return f(k(e));
-//       });
-//     }
-//   };
-// }
+/*
+Le code décisionnel et code impure (IO, del externes.. etc) sont séparés, les dependances sont données explicitement 
+
+Pros:
+- Le code d'infra/IO est injecté au moment d'appel de la fonction, donc pas compliqué à tester (le switch entre deps prod/test/dev/ etc c'est facile à faire)
+- Réduction de parametres necessaires 
+
+Cons:
+- Plus long à mettre en place (et même moins lisible)
+- Multiplie le nombre d'interfaces nécessaires
+- Multiplie le nombre de fonctions nécessaires
+- il est difficile de les coupler avec d’autres types d'implémentations (il est facile de tomber dans un «Type Tetris»)
+
+*/
 
 const pureUpdateCustomerProfileReader = (
   newProfile: Profile,
@@ -421,6 +419,73 @@ const updateCustomerProfileRM = (services: IServices) =>
         break;
     }
   }};
+
+type Reader<E, T> = (env: E) => Promise<T>;
+type App<A, E, T> = (a: A) => Reader<E, T>;
+
+const runReader = <E, T>(env: E, fa: Reader<E, T>): Promise<T> => fa(env);
+
+const pure = <E, T>(a: T): Reader<E, T> => () => Promise.resolve(a);
+
+const map = <E, A, B>(fa: Reader<E, A>, ab: (a: A) => B): Reader<E, B> => {
+  return (env: E) => runReader<E, A>(env, fa).then(ab);
+}
+
+const fmap = <E, A, B>(fa: Reader<E, A>, afb: (a: A) => Reader<E, B>): Reader<E, B> => {
+  return (env: E) => {
+    return runReader<E, A>(env, fa).then(a => 
+      runReader(env, afb(a))
+    );
+  }
+}
+
+
+const logInfo: App<string, IServices, void> = (message: string) => (env: IServices) => Promise.resolve(env.logger.Info(message));
+const update: App<Profile, IServices, void> = (profile: Profile) => async (env: IServices) =>  {
+  const dbConnection = defaultDbService.NewDbConnection();
+  await env.dbService.UpdateProfile(dbConnection)(profile)
+};
+const sendChangeNotification: App<EmailMessage, IServices, void> = (emailMessage: EmailMessage) =>  async (env: IServices) => {
+  await env.emailService.SendChangeNotification(defaultSmtpCredentials)(emailMessage);
+};
+
+const queryProfile: App<number, IServices, Profile> = (userId: number) => (env: IServices) => {
+  const dbConnection_ = env.dbService.NewDbConnection();
+  return env.dbService.QueryProfile(dbConnection_)(userId) as Promise<Profile>;
+}
+
+const sendEmailLog = logInfo('Sending email');
+
+const app: App<Profile, IServices, void> = (newProfile: Profile) => 
+  fmap(
+    queryProfile(newProfile.userId),
+    (currentProfile)=> {
+      if (currentProfile !== newProfile) {
+        return fmap(logInfo('Updating Profile'), () => {
+          if (currentProfile.emailAddress !== newProfile.emailAddress) {
+            const emailMessage: EmailMessage = {
+              To: newProfile.emailAddress,
+              Body: 'Please verify your email'
+            };
+            return fmap(sendEmailLog, () => 
+              fmap(update(newProfile), () => 
+                sendChangeNotification(emailMessage)
+              )
+            );
+          } else {
+            return update(newProfile);
+          }
+        });
+      } else {
+        return pure(undefined);
+      }
+
+    });
+
+const job = app(newProfileA);
+
+runReader(testServices, job);
+runReader(prodServices, job);
 
 // -----------------------------------------------------------------------------------------------
 // Dependency interpretation
@@ -492,72 +557,3 @@ const run = <T, R>(interpret: (instruction: T, next: (data: unknown) => void) =>
 };
 
 run(interpret)(app);
-
-
-
-type Reader<E, T> = (env: E) => Promise<T>;
-type App<A, E, T> = (a: A) => Reader<E, T>;
-
-const runReader = <E, T>(env: E, fa: Reader<E, T>): Promise<T> => fa(env);
-
-const pure = <E, T>(a: T): Reader<E, T> => () => Promise.resolve(a);
-
-const map = <E, A, B>(fa: Reader<E, A>, ab: (a: A) => B): Reader<E, B> => {
-  return (env: E) => runReader<E, A>(env, fa).then(ab);
-}
-
-const fmap = <E, A, B>(fa: Reader<E, A>, afb: (a: A) => Reader<E, B>): Reader<E, B> => {
-  return (env: E) => {
-    return runReader<E, A>(env, fa).then(a => 
-      runReader(env, afb(a))
-    );
-  }
-}
-
-
-const logInfo: App<string, IServices, void> = (message: string) => (env: IServices) => Promise.resolve(env.logger.Info(message));
-const update: App<Profile, IServices, void> = (profile: Profile) => async (env: IServices) =>  {
-  const dbConnection = defaultDbService.NewDbConnection();
-  await env.dbService.UpdateProfile(dbConnection)(profile)
-};
-const sendChangeNotification: App<EmailMessage, IServices, void> = (emailMessage: EmailMessage) =>  async (env: IServices) => {
-  await env.emailService.SendChangeNotification(defaultSmtpCredentials)(emailMessage);
-};
-
-const queryProfile: App<number, IServices, Profile> = (userId: number) => (env: IServices) => {
-  const dbConnection_ = env.dbService.NewDbConnection();
-  return env.dbService.QueryProfile(dbConnection_)(userId) as Promise<Profile>;
-}
-
-const sendEmailLog = logInfo('Sending email');
-
-const app: App<Profile, IServices, void> = (newProfile: Profile) => 
-  fmap(
-    queryProfile(newProfile.userId),
-    (currentProfile)=> {
-      if (currentProfile !== newProfile) {
-        return fmap(logInfo('Updating Profile'), () => {
-          if (currentProfile.emailAddress !== newProfile.emailAddress) {
-            const emailMessage: EmailMessage = {
-              To: newProfile.emailAddress,
-              Body: 'Please verify your email'
-            };
-            return fmap(sendEmailLog, () => 
-              fmap(update(newProfile), () => 
-                sendChangeNotification(emailMessage)
-              )
-            );
-          } else {
-            return update(newProfile);
-          }
-        });
-      } else {
-        return pure(undefined);
-      }
-
-    });
-
-const job = app(newProfileA);
-
-runReader(testServices, job);
-runReader(prodServices, job);
