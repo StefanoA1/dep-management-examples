89d863f11d59033d1512c125257526327dfcc337
stefano <stefanoacosta.92@gmail.com>
2021-02-23 11:56:22 +0100

    docs

--- src/workshop.ts
+++ src/workshop.ts
@@ -3,7 +3,8 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-empty-function */
 /**
- * Use case:
+ * 
+ Use case:
 Let’s look at a concrete use-case that we can use as a basis to experiment with different implementations.
 
 Say that we have some kind of web app with users, and each user has a “profile” with their name, email, preferences, etc. A use-case for updating their profile might be something like this:
@@ -13,6 +14,7 @@ Read the user’s current profile from the database
 If the profile has changed, update the user’s profile in the database
 If the email has changed, send a verification email message to the user’s new email
 We will also add a little bit of logging into the mix.
+
  */
 
 import * as R from 'fp-ts/lib/Reader';
@@ -543,14 +545,22 @@ runReader(prodServices, job);
 Le code décisionnel et code impure (IO, del externes.. etc) sont séparés, les dependances sont données explicitement 
 
 Pros:
-- Le code d'infra/IO est injecté avec l'interpreter
-- Réduction de parametres necessaires 
+- Le code d'infra/IO est toujours parametrizable si souhaité
+- Pas trop de parametres necessaires
+- L'interpreter devient interchangeable
+- Le code pure n'a pas besoin de gérer lui meme le code async (tout peut être compris dans l'interpreter)
 
 Cons:
-- Plus long à mettre en place (et même moins lisible)
+- Plus long à mettre en place (mais contrairement au Reader, c'est plus lisible)
 - Multiplie le nombre d'interfaces nécessaires
 - Multiplie le nombre de fonctions nécessaires
 - il est difficile de les coupler avec d’autres types d'implémentations (il est facile de tomber dans un «Type Tetris»)
+- Contrairement aux techniques de “dependency rejection” et “dependency parameterization”, qui ne nécessitent (presque) aucune 
+  connaissance particulière, les approches Reader et Interpreter demandent plus d'expertise.
+- Le debugging c'est plus complexe à suivre
+- Performance: structure de données très profondément imbriquée pour un programme large (ex: +1000 instructions), l'interprétation
+  peut être lente, utiliser beaucoup de mémoire, déclencher plus de garbage collection et même provoquer des stack overflows (il y a
+  des techniques pour soulager ça mais ça complifié le code encore plus - voir Trampolines: https://johnazariah.github.io/2020/12/07/bouncing-around-with-recursion.html#trampolines)
 
 */
 
@@ -559,10 +569,8 @@ type DbInstruction = ['Query', UserId] | ['Update', Profile];
 type EmailInstruction = ['SendChangeNotification', EmailMessage];
 type Instruction = LoggerInstruction | DbInstruction | EmailInstruction;
 
-const app = async function* (newProfile: Profile): AsyncGenerator<Instruction, void, unknown> {
-  const currentProfile = await ((yield ['Query', newProfile.userId]) as Promise<
-    Either<Error, Profile>
-  >);
+const app = function* (newProfile: Profile): Generator<Instruction, void, unknown> {
+  const currentProfile = (yield ['Query', newProfile.userId]) as Either<Error, Profile>;
 
   if (isRight(currentProfile) && currentProfile.right !== newProfile) {
     yield ['Info', 'Updating Profile'];
@@ -582,8 +590,9 @@ const app = async function* (newProfile: Profile): AsyncGenerator<Instruction, v
   }
 };
 
-const interpret = (services: IServices) => (
+const interpret = (services: IServices) => async (
   instruction: Instruction,
+  // eslint-disable-next-line @coorpacademy/coorpacademy/no-async-callback
   next: (data: unknown) => void
 ) => {
   switch (instruction[0]) {
@@ -597,33 +606,31 @@ const interpret = (services: IServices) => (
     }
     case 'Query': {
       const dbConnection = defaultDbService.NewDbConnection();
-      const currentProfile = services.dbService.QueryProfile(dbConnection)(instruction[1]);
+      const currentProfile = await services.dbService.QueryProfile(dbConnection)(instruction[1]);
       return next(currentProfile);
     }
     case 'Update': {
       const dbConnection = defaultDbService.NewDbConnection();
-      const currentProfile = services.dbService.UpdateProfile(dbConnection)(instruction[1]);
+      const currentProfile = await services.dbService.UpdateProfile(dbConnection)(instruction[1]);
       return next(currentProfile);
     }
     case 'SendChangeNotification': {
-      services.emailService.SendChangeNotification(defaultSmtpCredentials)(instruction[1]);
+      await services.emailService.SendChangeNotification(defaultSmtpCredentials)(instruction[1]);
       return next(undefined);
     }
   }
 };
 
-const run = <T, TR>(
-  _interpret: (_services: IServices) => (instruction: T, next: (data: unknown) => void) => void,
-  services: IServices
-) => (_app: AsyncGenerator<T, TR, unknown>) => {
-  const _interpreter = _interpret(services);
+const run = <T, TR>(_interpret: (instruction: T, next: (data: unknown) => void) => void) => (
+  _app: Generator<T, TR, unknown>
+) => {
   const loop = async (nextValue: unknown) => {
-    const {done, value} = await _app.next(nextValue);
+    const {done, value} = _app.next(nextValue);
     if (done) return value;
-    _interpreter(value as T, loop);
+    await _interpret(value as T, loop);
   };
 
   return loop(undefined);
 };
 
-await run(interpret, testServices)(app(newProfileA));
+await run(interpret(testServices))(app(newProfileA));
